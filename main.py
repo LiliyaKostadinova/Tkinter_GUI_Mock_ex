6ff27e642dbdefdde48257b021e87d2f41abc8ba
Lilia <costalilyy@gmail.com>
2023-01-04 13:06:15 +0200

    Docstr, ttk style & super fix

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ LIGHT_BLUE = '#e3f8fb'
 
 # Matplotlib logic
 class PlotView:
-    """A class that handles parsing the JSON file and visualizing the graph"""
+    """Handle parsing the JSON file and visualize the graph"""
     def __init__(self, file_name):
-        """Getting the file path, opening it and calling a method to load the information"""
+        """Constructor"""
         self.file_name = file_name
         self.json_f = open(file_name)
         self.load_info()
 
     def load_info(self):
-        """Loading data from the file, parsing it to according variables, calling а method to visualize the plot"""
+        """Load data from the file, parse it to according variables, call a method to visualize the plot"""
         info = json.load(self.json_f)
         plot_title = info['title']
         plot_line_label = info['label']
@@ -27,7 +27,7 @@ class PlotView:
         self.show_plot(plot_title, y_data, plot_line_label, line_color)
 
     def show_plot(self, plot_title, y_data, plot_line_label, line_color):
-        """Visualizing plot using matplotlib, closing file"""
+        """Visualize plot using matplotlib, close file"""
         plt.plot(y_data, label=plot_line_label, color=line_color)
         plt.title(plot_title)
         plt.xlabel('X axis')
@@ -40,35 +40,36 @@ class PlotView:
 
 # Tkinter interface setup
 class Root(tk.Tk):
-    """A class that handles the initial window by using Tkinter for visualizing"""
+    """Handle the initial window by using Tkinter for visualization"""
     def __init__(self):
-        """Setting up font styles, titles and elements"""
-        super(Root, self).__init__()
+        """Constructor"""
+        super().__init__()
         self.configure(background=LIGHT_BLUE)
-        self.lab_text_style = ttk.Style()
-        self.lab_text_style.configure('text_style.TLabel', font=('Helvetica', 12), background=LIGHT_BLUE)
-        self.btn_text_style = ttk.Style()
-        self.btn_text_style.configure('text_style.TButton', font=('Helvetica', 10))
+        self.text_style = ttk.Style()
+        self.text_style.configure('text_style.TLabel', font=('Helvetica', 12), background=LIGHT_BLUE)
         self.title('Linear plot')
+
         self.choose_label = ttk.Label(text='Choose a JSON file:', style='text_style.TLabel')
         self.choose_label.pack(padx=50, pady=(50, 25))
+
         self.br_button = self.browse_button()
         self.file_name = None
 
     def browse_button(self):
-        """Creating a button that calls a browse-file functionality on click"""
+        """Create a button that calls a browse-file functionality on click"""
+        self.text_style.configure('text_style.TButton', font=('Helvetica', 10))
         self.br_button = ttk.Button(self, text='Browse', style='text_style.TButton', command=self.open_file)
         self.br_button.pack(pady=(0, 50))
         return self.br_button
 
     def open_file(self):
-        """Handling file browsing, passing the file path by initiating an instance of PlotView class"""
+        """Handle file browsing, pass the file path by initiating an instance of PlotView class"""
         self.file_name = filedialog.askopenfile(title='Search file')
         if self.file_name:
             new_plot = PlotView(self.file_name.name)
 
 
 if __name__ == '__main__':
-    """Running the program by starting the Tkinter interface"""
+    """Run the program by starting the Tkinter interface"""
     root = Root()
     root.mainloop()
